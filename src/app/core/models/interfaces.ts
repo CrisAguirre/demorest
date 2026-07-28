5a2f8d36a7d85e22994f655f068aedd4f054a94a
Cris <krontroth@gmail.com>
2026-07-27 22:44:18 -0500

    54654

--- src/app/core/models/interfaces.ts
+++ src/app/core/models/interfaces.ts
@@ -3,7 +3,7 @@
 
 export interface User {
   _id: string; name: string; email: string;
-  role: 'admin' | 'cajero' | 'cliente';
+  role: 'admin' | 'cajero' | 'cliente' | 'cocinero' | 'mesero';
   isActive: boolean; createdAt: string; phone?: string; address?: string;
 }
 export interface AuthResponse { user: User; accessToken: string; refreshToken: string; }
